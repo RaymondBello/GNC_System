237d440f83b5792fc5e2c6686aff33a026c1525b
Raymond Bello <raymondbello11@gmail.com>
2022-07-02 23:20:58 -0400

    added spi

--- rasbian/app/main.cpp
+++ rasbian/app/main.cpp
@@ -30,10 +30,21 @@ int main() {
             << "."
             << PROJECT_VERSION_TWEAK
             << std::endl;
-  // std::system("cat ../LICENSE");
 
-  // Bring in the dummy class from the example source,
-  // just to show that it is accessible from main.cpp.
-  Dummy d = Dummy();
-  return d.doSomething() ? 0 : -1;
+  int fd = wiringPiSPISetupMode(SPI_CHANNEL, SPI_CLOCK_SPEED, 0);
+  if (fd == -1)
+  {
+    std::cout << "Failed to init SPI communication.\n";
+    return -1;
+  }
+  std::cout << "SPI communication successfully setup.\n";
+
+  unsigned char buf[2] = {23, 0};
+  wiringPiSPIDataRW(SPI_CHANNEL, buf, 2);
+  std::cout << "Data returned: " << +buf[1] << "\n";
+  
+  return 0;
+
 }
+
+
